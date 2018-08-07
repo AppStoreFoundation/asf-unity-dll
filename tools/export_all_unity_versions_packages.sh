0a14aa238ad4b1019b42d6902556b6a307ef811a
Fábio Vital <fabio.vital@aptoide.com>
2018-08-07 10:35:32 +0100

    ignore setuo env vars sh file

--- tools/export_all_unity_versions_packages.sh
+++ tools/export_all_unity_versions_packages.sh
@@ -3,12 +3,12 @@ source setup_env_vars.sh
 
 echo "export package for Unity 5.6"
 cd $PROJ_5_6_PATH
-sh defaultExport.sh
+sh .defaultExport.sh
 
 echo "export package for Unity 2017"
 cd $PROJ_2017_PATH
-sh defaultExport.sh
+sh .defaultExport.sh
 
 echo "export package for Unity 2018"
 cd $PROJ_2018_PATH
-sh defaultExport.sh
+sh .defaultExport.sh
--- tools/setup_env_vars.sh
+++
@@ -1,4 +0,0 @@
-#bash
-PROJ_5_6_PATH=/Users/nunomonteiro/Documents/GitHub/AppcoinsUnityPlugin5_6/
-PROJ_2017_PATH=/Users/nunomonteiro/Documents/GitHub/AppcoinsUnityPlugin2017/
-PROJ_2018_PATH=/Users/nunomonteiro/Documents/GitHub/AppcoinsUnityPlugin/
